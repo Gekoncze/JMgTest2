23a17d7e2a16fee19bb3a711ebc7a25fd9267f75
Mg <gekoncze@seznam.cz>
2023-03-10 18:10:09 +0100

    added assert equals for collections

--- src/cz/mg/test/Assert.java
+++ src/cz/mg/test/Assert.java
@@ -1,7 +1,10 @@
 package cz.mg.test;
 
+import cz.mg.annotations.classes.Utility;
 import cz.mg.annotations.requirement.Mandatory;
 import cz.mg.annotations.requirement.Optional;
+
+import java.util.Iterator;
 
 @SuppressWarnings("unchecked")
 public class Assert {
@@ -103,5 +106,79 @@ public class Assert {
                 "Unexpected exception of type " + e.getClass().getSimpleName() + " with message: " + e.getMessage(), e
             );
         }
+    }
+
+    public static <T> void assertEquals(
+        @Optional Iterable<T> expectation,
+        @Optional Iterable<T> reality,
+        @Mandatory CompareFunction<T> compareFunction,
+        @Mandatory ToStringFunction<T> toStringFunction
+    ) {
+        if (expectation == reality) {
+            return;
+        }
+
+        if (expectation == null) {
+            throw new AssertException("Expected list to be null, but was not null.");
+        }
+
+        if (reality == null) {
+            throw new AssertException("Expected list to not be null, but was null.");
+        }
+
+        int expectedCount = count(expectation);
+        int actualCount = count(reality);
+
+        if (expectedCount != actualCount) {
+            throw new AssertException("Expected list count to be " + expectedCount + ", but was " + actualCount + ".");
+        }
+
+        int i = -1;
+        Iterator<T> expectationIterator = expectation.iterator();
+        Iterator<T> realityIterator = reality.iterator();
+        while (expectationIterator.hasNext() && realityIterator.hasNext()) {
+            i++;
+            @Optional T expectedItem = expectationIterator.next();
+            @Optional T actualItem = realityIterator.next();
+
+            if (expectedItem == actualItem) {
+                continue;
+            }
+
+            if (expectedItem == null) {
+                throw new AssertException(
+                    "Expected item at " + i + " to be null, but was " + toStringFunction.toString(actualItem) + "."
+                );
+            }
+
+            if (actualItem == null) {
+                throw new AssertException(
+                    "Expected item at " + i + " to be " + toStringFunction.toString(expectedItem) + ", but was null."
+                );
+            }
+
+            if (!compareFunction.equals(expectedItem, actualItem)) {
+                throw new AssertException(
+                    "Expected item at " + i + " to be " + toStringFunction.toString(expectedItem) + ", " +
+                        "but was " + toStringFunction.toString(actualItem) + "."
+                );
+            }
+        }
+    }
+
+    private static <T> int count(Iterable<T> iterable) {
+        int count = 0;
+        for (T ignored : iterable) {
+            count++;
+        }
+        return count;
+    }
+
+    public @Utility interface CompareFunction<T> {
+        boolean equals(@Mandatory T a, @Mandatory T b);
+    }
+
+    public @Utility interface ToStringFunction<T> {
+        String toString(@Mandatory T a);
     }
 }
