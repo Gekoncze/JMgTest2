0c0cacdf49a3e98e409b4bb1382ab3a8cf374eca
mg <gekoncze@seznam.cz>
2025-09-03 12:26:56 +0200

    added quick code assert

--- src/cz/mg/test/Assert.java
+++ src/cz/mg/test/Assert.java
@@ -3,22 +3,13 @@ package cz.mg.test;
 import cz.mg.annotations.classes.Static;
 import cz.mg.annotations.requirement.Mandatory;
 import cz.mg.annotations.requirement.Optional;
+import cz.mg.test.components.UnsafeRunnable;
 import cz.mg.test.components.builders.BinaryCollectionAssertion;
 import cz.mg.test.components.builders.BinaryObjectAssertion;
+import cz.mg.test.components.builders.FluentCodeAssertion;
 import cz.mg.test.exceptions.AssertException;
 
 public @Static class Assert {
-    public static @Mandatory <T> BinaryObjectAssertion<T> assertThat(@Optional T expectation, @Optional T reality) {
-        return new BinaryObjectAssertion<>(expectation, reality);
-    }
-
-    public static @Mandatory <T> BinaryCollectionAssertion<T> assertThatCollections(
-        @Optional Iterable<T> expectation,
-        @Optional Iterable<T> reality
-    ) {
-        return new BinaryCollectionAssertion<>(expectation, reality);
-    }
-
     public static void assertNull(@Optional Object object) {
         if (object != null) {
             throw new AssertException("Expected null, but got " + object + ".");
@@ -45,5 +36,24 @@ public @Static class Assert {
 
     public static void assertNotEquals(@Optional Object expectation, @Optional Object reality) {
         assertThat(expectation, reality).areNotEqual();
+    }
+
+    public static void assertException(@Mandatory UnsafeRunnable runnable, @Mandatory Class<? extends Exception> type) {
+        new FluentCodeAssertion(runnable).throwsException(type);
+    }
+
+    public static void assertNoException(@Mandatory UnsafeRunnable runnable) {
+        new FluentCodeAssertion(runnable).doesNotThrowAnyException();
+    }
+
+    public static @Mandatory <T> BinaryObjectAssertion<T> assertThat(@Optional T expectation, @Optional T reality) {
+        return new BinaryObjectAssertion<>(expectation, reality);
+    }
+
+    public static @Mandatory <T> BinaryCollectionAssertion<T> assertThatCollections(
+        @Optional Iterable<T> expectation,
+        @Optional Iterable<T> reality
+    ) {
+        return new BinaryCollectionAssertion<>(expectation, reality);
     }
 }
